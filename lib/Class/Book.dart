72b581da6de84a2d645a729130eda7ca0296d904
rafa <rafa.zamora.rals@gmail.com>
2020-02-24 18:40:43 -0600

    Clase Book

---
+++ lib/Class/Book.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class Book {
+  String title;
+  String editorial;
+  List<String> autors;
+  String year;
+  String edition;
+  String library;
+  String themes;
+  String key;
+  String availability;
+  String location;
+  String image;
+  String review;
+  String stars;
+  String format;
+  String apa;
+  Color colorTop;
+  Color colorEnd;
+  Color colorAcent;
+  Color colorText;
+
+  Book({
+    this.autors,
+    this.availability,
+    this.edition,
+    this.editorial,
+    this.key,
+    this.library,
+    this.location,
+    this.themes,
+    this.title,
+    this.year,
+    this.image,
+    this.colorTop,
+    this.colorEnd,
+    this.colorAcent,
+    this.colorText,
+    this.review,
+    this.stars,
+    this.format,
+    this.apa,
+  });
+
+  Book.fromJson(Map<String, dynamic> json) {
+    autors = json['autors'];
+    availability = json['availability'];
+    edition = json['edition'];
+    editorial = json['editorial'];
+    key = json['key'];
+    library = json['library'];
+    location = json['location'];
+    themes = json['themes'];
+    title = json['title'];
+    year = json['year'];
+    image = json['image'];
+    format = json['format'];
+    /*
+    colorTop = json['colorTop']; 
+    colorEnd = json['colorEnd']; 
+    colorAcent = json['colorAcent']; 
+    colorText = json['colorText']; 
+    */
+    review = json['review'];
+    stars = json['stars'];
+  }
+
+  void Print() {
+    print(this.title);
+    print(this.autors.toString());
+    print(this.library.toString());
+    print(this.edition);
+    print(this.themes);
+    print(this.availability);
+    print(this.editorial);
+    print(this.location);
+    print(this.year);
+    print(this.key);
+  }
+}
