589d35fe5cd652e015fcfcd94b207b3327723b01
rafa <rafa.zamora.rals@gmail.com>
2020-02-24 18:43:47 -0600

    Icono del SplashScreen

---
+++ lib/Screens/SplashScreen/ImageIcon.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart' as animate;
+class ImageIcon extends StatelessWidget {
+  const ImageIcon({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        width: 150,
+        height: 150,
+        child: Center(
+          ///Icono
+          child: animate.BounceInDown(
+            duration: Duration(seconds: 2),
+            child: Image.asset('assets/placeholderImage.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
